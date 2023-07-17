5973fd451449e890b52f48e4a5861f754f1d4bd2
Elen Tara <elentara220@gmail.com>
2023-07-17 13:14:39 +0300

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Elena Tarasenko and is{" "}
-        <a
-          href="https://github.com/ElenTara/react-week-5-weather-app"
-          target="_blank"
-          rel="noopener"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Elena Tarasenko and is{" "}
+          <a
+            href="https://github.com/ElenTara/react-week-5-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
